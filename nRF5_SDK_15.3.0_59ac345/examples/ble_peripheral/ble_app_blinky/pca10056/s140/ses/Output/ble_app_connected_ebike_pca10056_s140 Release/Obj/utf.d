d568bbc795f4276156a4db168db85746955e1221
jonGetUp <jo.gaspoz@gmail.com>
2020-08-09 14:46:06 +0200

    change name

---
+++ nRF5_SDK_15.3.0_59ac345/examples/ble_peripheral/ble_app_blinky/pca10056/s140/ses/Output/ble_app_connected_ebike_pca10056_s140 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_connected_ebike_pca10056_s140\ Release/Obj/utf.o: \
+ C:\Users\jogas\Google\ Drive\Bachelor\_Software\nRF52840\nRF-WirelessCycle\nRF5_SDK_15.3.0_59ac345\external\utf_converter\utf.c \
+ C:\Users\jogas\Google\ Drive\Bachelor\_Software\nRF52840\nRF-WirelessCycle\nRF5_SDK_15.3.0_59ac345\external\utf_converter\utf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdint.h
